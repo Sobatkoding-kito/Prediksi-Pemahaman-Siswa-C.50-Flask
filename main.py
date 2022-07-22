5fae4e621929568aac1137715f5055960ca2a118
Haxorsprogramming <haxors@uinsu.ac.id>
2022-07-22 15:34:11 +0700

    update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, url_for, render_template, request, jsonify 
-import pandas
+import pandas as pd
 from sklearn import tree
 import pydotplus
 from sklearn.tree import DecisionTreeClassifier
@@ -12,11 +12,34 @@ import graphviz
 
 app = Flask(__name__)
 
+BASE_URL = "http://127.0.0.1:5000/"
+
 # dataSiswa = pd.read_csv("tbl_data_latih.csv", sep = ';', encoding = 'UTF-8')
+appName = ''
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    dr = {'BASE_URL' : BASE_URL}
+    return render_template('home.html', dRes=dr)
+
+@app.route('/data-siswa')
+def data_siswa():
+    dSiswa = []
+    dataSiswa = pd.read_csv("tbl_data_latih.csv", sep = ';', encoding = 'UTF-8')
+    dtnp = dataSiswa.to_numpy()
+    # print(dtnp)
+    ord = 1
+    for x in dtnp:
+        dSatuan = {}
+        dSatuan['nama'] = x[2]
+        dSatuan['ord'] = ord
+        ord += 1
+        dSiswa.append(dSatuan)
+        # print(x[2])
+    dtnp = []
+    dr = {'dataSiswa' : dSiswa}
+    # print(dSiswa)
+    return render_template('data-siswa.html', dataSiswa=dSiswa)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
